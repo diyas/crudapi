f6fda8aebc2c070c88da832cd1e9c66c137549eb
diyas <diazch4ndra@gmail.com>
2019-04-12 18:34:11 +0700

    Crud API

--- src/main/java/com/app/crudapi/model/Paginate.java
+++ src/main/java/com/app/crudapi/model/Paginate.java
@@ -1,41 +1,11 @@
 package com.app.crudapi.model;
 
+import lombok.Data;
+
+@Data
 public class Paginate {
     private int page;
     private int limit;
     private long totalData;
     private long totalPage;
-
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-
-    public long getTotalData() {
-        return totalData;
-    }
-
-    public void setTotalData(long totalData) {
-        this.totalData = totalData;
-    }
-
-    public long getTotalPage() {
-        return totalPage;
-    }
-
-    public void setTotalPage(long totalPage) {
-        this.totalPage = totalPage;
-    }
 }
