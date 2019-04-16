537241266cf69e12534e45c39b619a882e65e4b5
diyas <diazch4ndra@gmail.com>
2019-04-16 17:21:50 +0700

    Crud API

--- src/main/java/com/app/crudapi/CrudapiApplication.java
+++ src/main/java/com/app/crudapi/CrudapiApplication.java
@@ -1,13 +1,28 @@
 package com.app.crudapi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CrudapiApplication {
 
+@SpringBootApplication
+public class CrudapiApplication implements ApplicationRunner {
+
+	private static final Logger logger = LogManager.getLogger(CrudapiApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CrudapiApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
